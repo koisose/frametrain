5df709a6335c92f151697f0d5bb2f581a72b06cd
koisose <plokotokbelajar@gmail.com>
2024-10-07 19:52:25 +0700

    🚀fix(templates/superfluid/Inspector.tsx): Fix Farcaster user selection Fixing the functionality of selecting a farcaster user. Now, the application correctly selects and updates the selected farcaster user\'s information when switching between "yourself" and "someone else". The farcaster user\'s information is also updated accordingly in the address dropdown.

--- templates/superfluid/Inspector.tsx
+++ templates/superfluid/Inspector.tsx
@@ -1,25 +1,28 @@
 'use client'
 import { Button, Input, Select } from '@/sdk/components'
-import type {  FarcasterUserInfo } from '@/lib/farcaster'
-import { useFrameConfig,useFarcasterId,useFarcasterName } from '@/sdk/hooks'
+import type { FarcasterUserInfo } from '@/lib/farcaster'
+import { useFrameConfig, useFarcasterId, useFarcasterName } from '@/sdk/hooks'
 import { getFarcasterProfiles } from '@/sdk/neynar'
 import { Configuration } from '@/sdk/inspector'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import type { Config } from '.'
 
 export default function Inspector() {
     const [config, updateConfig] = useFrameConfig<Config>()
     const [username, setUsername] = useState("")
-    const [allUser,setAllUser]=useState<FarcasterUserInfo[]>([])
-    const farId=useFarcasterId()
-    const farName=useFarcasterName()
+    const [allUser, setAllUser] = useState<FarcasterUserInfo[]>([])
+    const farId = useFarcasterId()
+    const farName = useFarcasterName()
     const { who } = config
 
-    useEffect(() => {
-        if (who==='yourself'){
+    useEffect(()=>{
+        if(who==="yourself"){
             getFarcasterProfiles([farId]).then(user=>setAllUser(user))
+        }else{
+            setAllUser([])
         }
-    }, [who])
+        
+    },[who])
 
     return (
         <Configuration.Root>
@@ -27,27 +30,40 @@ export default function Inspector() {
                 <h2 className="text-lg font-semibold">Set Farcaster Username</h2>
 
                 <Select
-                    defaultValue={config?.who || 'yourself'}
-                    onChange={(value) => {
+                    defaultValue={config?.who}
+                    onChange={async (value) => {
                         const who = value as 'yourself' | 'someone'
                         updateConfig({ who })
+                        if (who === "yourself") {
+                            const profile=await getFarcasterProfiles([farId]);
+                            setAllUser(profile)
+                            updateConfig({ username: profile[0].username, profpic: profile[0].pfp_url })
+                        }else{
+                            updateConfig({ username: "", address: "",profpic:"" })
+                        }
                     }}
                 >
+                    <option value=""></option>
                     <option value="yourself">Yourself {`(${farName})`}</option>
                     <option value="someone">Someone else</option>
                 </Select>
-                {/* {allUser[0].verifications} */}
-                {who==='yourself' && allUser.length>0 && <>
-                                <h2 className="text-lg font-semibold">Pick Address to Send to</h2>
-                                <Select
-                   
-                >
-                    <option value={allUser[0].custody_address}>{allUser[0].custody_address}</option>
-                    <option value={allUser[0].verifications}>{allUser[0].verifications}</option>
-                 
-                </Select>
+              
+                {who === 'yourself' && allUser && allUser.length > 0 && <>
+                    <h2 className="text-lg font-semibold">Pick Address to Send to</h2>
+                    <Select
+                        defaultValue={config?.address}
+                        onChange={async(value) => {
+                            const profile=await getFarcasterProfiles([farId]);
+                            setAllUser(profile)
+                            updateConfig({ address: value, username: profile[0].username, profpic: allUser[0].pfp_url })
+                        }}
+                    >
+                        <option value={allUser[0].custody_address}>{allUser[0].custody_address}</option>
+                        <option value={allUser[0].verifications}>{allUser[0].verifications}</option>
+
+                    </Select>
                 </>}
-                {config.who === 'someone' && <Input
+                {who === 'someone' && <Input
                     className="py-2 text-lg"
                     placeholder="username"
                     defaultValue={username}
